11bcd576fc084474e1f61038d0cfa6f49ce9ad6c
SHREYASREENATH <deepa.dshreya@gmail.com>
2022-01-11 20:10:20 +0530

    Sorce Code of digital Clock added

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ root = Tk()
 root.title("clock")
 
 def time():
+    # To dispaly the time in hours minutes seconds and %p stands for a.m, p.m
     string = strftime('%H:%M:%S %p')
     label.config(text=string)
+    # To set the delay time-To change after 1 sec
     label.after(1000, time)
-
+# defining the interface
 label = Label(root, font=("DS-digital", 80), background="black", foreground="violet")
+# allign them to centre
 label.pack(anchor='center')
 time()
 mainloop()
